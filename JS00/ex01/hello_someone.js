f3b15bd05f026432557d80ab699b7331691ec28f
yujo <yujo@student.42seoul.kr>
2020-06-19 18:48:31 +0900

    JS00 ex01 modified problem solved

--- JS00/ex01/hello_someone.js
+++ JS00/ex01/hello_someone.js
@@ -1,15 +1,21 @@
 export default function hello_someone(name) {
-  if (typeof name === "number" || typeof name === "string") {
-    console.log(`Hello ${name}!`);
+  if (Number.isNaN(name) === true) {
+    console.log("Age is just a number");
+  } else if (typeof name === "string") {
+    if (name === "") {
+      console.log("Who are you?");
+    } else {
+      console.log(`My name is ${name}`);
+    }
+  } else if (typeof name === "number") {
+    if (name <= 0) {
+      console.log("I`m Benjamin Button");
+    } else {
+      console.log(`My age is ${name}`);
+    }
+  } else if (typeof name === "undefined") {
+    console.log("Nobody can define me!");
   } else if (typeof name === "object") {
-    console.log("Who are you?");
-  } else {
-    throw Error;
+    console.log("I am null and void");
   }
 }
-
-// test code
-
-// hello_someone("asd");
-// hello_someone(123);
-// hello_someone(null);
--- JS00/ex01/test.js
+++ JS00/ex01/test.js
@@ -1,3 +1,9 @@
 import hello_someone from "./hello_someone.js";
 
-hello_someone("testing");
+hello_someone("42");
+hello_someone("");
+hello_someone(null);
+hello_someone(42);
+hello_someone(-1);
+hello_someone(NaN);
+hello_someone(undefined);
