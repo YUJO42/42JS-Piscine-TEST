6bf9af100b68d6a62d1c96e9ade05bede1e5a3b3
yujo <yujo@student.42seoul.kr>
2020-08-21 12:10:39 +0900

    FP00 - ex00 clean up

--- FP00/ex00/ft.js
+++ FP00/ex00/ft.js
@@ -1,26 +1,3 @@
 const ft = {
-  //   log(param) {
-  //     console.log(param);
-  //   },
   log: console.log,
 };
-
-const chicken_vs_egg = (param) => () => {
-  if (param === "egg") {
-    return "chicken";
-  } else if (param === "chicken") {
-    return "egg";
-  }
-};
-
-ft.log(2);
-ft.log("Good Morning!");
-ft.log(ft.log === console.log);
-
-ft.log(chicken_vs_egg("egg"));
-ft.log(chicken_vs_egg("chicken"));
-
-console.log("-------------------------");
-
-ft.log(chicken_vs_egg("egg")());
-ft.log(chicken_vs_egg("chicken")());
--- FP00/ex00/test.js
+++ FP00/ex00/test.js
@@ -1,9 +1,29 @@
 const ft = {
-  log(param) {
-    console.log(param);
-  },
+  log: console.log,
 };
+
+const chicken_vs_egg = (param) => () => {
+  if (param === "egg") {
+    return "chicken";
+  } else if (param === "chicken") {
+    return "egg";
+  }
+};
+
+ft.log("-------------------------");
 
 ft.log(2);
 ft.log("Good Morning!");
 ft.log(ft.log === console.log);
+
+ft.log("-------------------------");
+
+ft.log(chicken_vs_egg("egg"));
+ft.log(chicken_vs_egg("chicken"));
+
+ft.log("-------------------------");
+
+ft.log(chicken_vs_egg("egg")());
+ft.log(chicken_vs_egg("chicken")());
+
+ft.log("-------------------------");
